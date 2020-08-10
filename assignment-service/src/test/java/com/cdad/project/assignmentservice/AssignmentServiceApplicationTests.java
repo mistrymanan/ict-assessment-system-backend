3ba57ae60de2e3cafb3ead299e19ff29d3fde3e5
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2020-08-10 19:52:10 +0530

    project-init

---
+++ assignment-service/src/test/java/com/cdad/project/assignmentservice/AssignmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdad.project.assignmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ grading-service/src/test/java/com/cdad/project/gradingservice/GradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdad.project.gradingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
