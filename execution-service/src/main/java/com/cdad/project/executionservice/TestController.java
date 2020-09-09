68e17b1b4d9290a271174ebb6b7d4f09618a3ce0
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2020-09-09 22:37:53 +0530

    replaced execution-service

---
+++ execution-service/src/main/java/com/cdad/project/executionservice/TestController.java
@@ -0,0 +1,14 @@
+package com.cdad.project.executionservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("")
+public class TestController {
+  @GetMapping("{name}")
+  @ResponseBody
+  public String helloMessage(@PathVariable String name) {
+    return "<h1>Hello " + name + "</h1>";
+  }
+}
