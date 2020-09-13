349951434c8a82753c37bbe9021f0a6c3be57381
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2020-09-13 23:04:01 +0530

    dekorate configured

--- assignment-service/src/main/java/com/cdad/project/assignmentservice/AssignmentServiceApplication.java
+++ assignment-service/src/main/java/com/cdad/project/assignmentservice/AssignmentServiceApplication.java
@@ -2,6 +2,10 @@ package com.cdad.project.assignmentservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.annotation.Label;
+import io.dekorate.kubernetes.annotation.Port;
+import io.dekorate.kubernetes.annotation.ServiceType;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +16,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@KubernetesApplication(
+        labels = @Label(key = "app", value = "assignment-service"),
+        replicas = 2,
+        expose = true,
+        ports = @Port(name="http",containerPort = 8080),
+        serviceType = ServiceType.NodePort
+)
 public class AssignmentServiceApplication {
 
   @Bean
