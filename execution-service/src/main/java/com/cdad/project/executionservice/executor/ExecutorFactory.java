714b055372da3c36776f6b6b431b610c37415902
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2020-09-19 18:09:02 +0530

    c-executor-Added

--- execution-service/src/main/java/com/cdad/project/executionservice/executor/ExecutorFactory.java
+++ execution-service/src/main/java/com/cdad/project/executionservice/executor/ExecutorFactory.java
@@ -2,6 +2,7 @@ package com.cdad.project.executionservice.executor;
 
 import com.cdad.project.executionservice.entity.Language;
 import com.cdad.project.executionservice.entity.Program;
+import com.cdad.project.executionservice.executor.compiled.CExecutor;
 import com.cdad.project.executionservice.executor.compiled.JavaExecutor;
 import com.cdad.project.executionservice.executor.interpreted.PythonExecutor;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ public class ExecutorFactory {
         Executor executor;
         if(program.getLanguage()==Language.JAVA) return new JavaExecutor(program);
         else if(program.getLanguage()==Language.PYTHON) return new PythonExecutor(program);
+        else if(program.getLanguage()==Language.C) return new CExecutor(program);
         return null;
     }
 }
--- execution-service/src/main/java/com/cdad/project/executionservice/executor/compiled/CExecutor.java
+++ execution-service/src/main/java/com/cdad/project/executionservice/executor/compiled/CExecutor.java
@@ -1,2 +1,63 @@
-package com.cdad.project.executionservice.executor.compiled;public class CExecutor {
+package com.cdad.project.executionservice.executor.compiled;
+
+import com.cdad.project.executionservice.entity.Program;
+import com.cdad.project.executionservice.entity.Status;
+import com.cdad.project.executionservice.executor.BaseExecutor;
+import lombok.Data;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CExecutor extends BaseExecutor implements CompiledExecutor {
+    public CExecutor(Program program) throws IOException, InterruptedException {
+        super(program);
+        if(!this.compile().equals(0)){
+            this.status=Status.COMPILATIONERROR;
+        }
+    }
+
+    @Override
+    public Integer compile() throws IOException, InterruptedException {
+        List<String> command=new ArrayList<String>();
+        //command.add("sudo chroot /jail/ echo");
+        command.add("cd /jail"+getBaseExecutionPath()+"/ ");
+        command.add("gcc Solution.c");
+        ProcessBuilder processBuilder=new ProcessBuilder().command("/bin/bash","-c",String.join(";",command))
+                .directory(new File(getJailPath()))
+                .redirectError(new File(this.getContainerPath()+"error.txt"));
+        Process process=processBuilder.start();
+        Integer statusCode=process.waitFor();
+        return statusCode;
+    }
+
+    @Override
+    public Status run() throws InterruptedException, IOException {
+        if(this.getStatus()!=null&&this.getStatus().equals(Status.COMPILATIONERROR)) return this.getStatus();
+
+        long startTime=System.nanoTime();
+        List<String> command=new ArrayList<String>();
+        command.add("sudo chroot /jail/ echo");
+        command.add("cd c/"+this.getUniquePath()+"/ ");
+        command.add("cat input.txt | timeout "+this.getTimeout()+" ./a.out");
+        ProcessBuilder processBuilder=new ProcessBuilder().command("/bin/bash","-c",String.join(" ; ",command))
+                .directory(new File(getJailPath()))
+                .redirectOutput(new File(getContainerPath()+"output.txt"))
+                .redirectError(new File(getContainerPath()+"error.txt"));
+        Process process=processBuilder.start();
+        Integer statusCode=process.waitFor();
+        long endTime=System.nanoTime();
+        setExecutionTime(endTime-startTime);
+        this.setStatusBasedOnStatusCode(statusCode);
+        return this.status;
+    }
+    public void setStatusBasedOnStatusCode(Integer statusCode) {
+        if (statusCode.equals(0)) { this.status=Status.SUCCEED;}
+        else if(statusCode.equals(143) || statusCode.equals(124)){
+            this.status=Status.TIMEOUT;
+        }
+        else{this.status=Status.RUNTIMEERROR;}
+    }
 }
