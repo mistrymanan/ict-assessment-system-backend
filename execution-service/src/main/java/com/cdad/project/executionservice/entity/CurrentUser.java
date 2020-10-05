43d159e589d4d2fc3b7e8637d540804e8e956c39
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2020-10-05 17:07:44 +0530

    configured-as-oauth2-resource-server

---
+++ execution-service/src/main/java/com/cdad/project/executionservice/entity/CurrentUser.java
@@ -0,0 +1,21 @@
+package com.cdad.project.executionservice.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+@Data
+@ToString
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CurrentUser {
+  private String name;
+  private String email;
+
+  public static CurrentUser fromJwt(Jwt jwt) {
+    String name = jwt.getClaimAsString("name");
+    String email = jwt.getClaimAsString("email");
+    return new CurrentUser(name, email);
+  }
+}
