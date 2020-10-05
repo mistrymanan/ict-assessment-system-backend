82007832ec62094a2e88a6082f637428373c7224
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2020-10-05 09:07:25 +0530

    404-handled

--- execution-service/src/main/java/com/cdad/project/executionservice/dto/ErrorResponse.java
+++ execution-service/src/main/java/com/cdad/project/executionservice/dto/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.cdad.project.executionservice.dto;
 
 import com.cdad.project.executionservice.entity.Status;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private Status status;
     private String message;
