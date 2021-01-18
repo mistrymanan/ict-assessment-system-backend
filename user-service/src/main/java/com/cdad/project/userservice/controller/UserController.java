46b5f78126185c8f49457aaed44b6e637dccf1af
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-18 10:14:41 +0530

    user-controller added

--- user-service/src/main/java/com/cdad/project/userservice/controller/UserController.java
+++ user-service/src/main/java/com/cdad/project/userservice/controller/UserController.java
@@ -1,2 +1,8 @@
-package com.cdad.project.userservice.controller;public class UserController {
+package com.cdad.project.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("")
+public class UserController {
+
 }
