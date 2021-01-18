52aa49af3b1a14ef049454258e1aeb0c1a54c802
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-18 11:57:47 +0530

    user controller added

--- user-service/src/main/java/com/cdad/project/userservice/controller/UserController.java
+++ user-service/src/main/java/com/cdad/project/userservice/controller/UserController.java
@@ -1,8 +1,42 @@
 package com.cdad.project.userservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.cdad.project.userservice.dto.ErrorResponse;
+import com.cdad.project.userservice.entity.User;
+import com.cdad.project.userservice.exceptions.UserNotFoundException;
+import com.cdad.project.userservice.exchanges.CreateUserRequest;
+import com.cdad.project.userservice.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequestMapping("")
 public class UserController {
+    final private UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    User createUser(@Valid CreateUserRequest user, @AuthenticationPrincipal Jwt jwt){
+        return userService.save(user);
+    }
+    @GetMapping("{emailId}")
+    User getUser(@PathVariable String emailId, @AuthenticationPrincipal Jwt jwt) throws UserNotFoundException {
+        return userService.getByEmailId(emailId);
+    }
+    @PatchMapping("{emailId}")
+    User updateUserInfo(@PathVariable String emailId, @AuthenticationPrincipal Jwt jwt) throws UserNotFoundException {
+    return userService.updateUserMetadata(emailId, jwt);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handle(Exception e) {
+        return new ErrorResponse("Not Found", e.getMessage());
+    }
 }
