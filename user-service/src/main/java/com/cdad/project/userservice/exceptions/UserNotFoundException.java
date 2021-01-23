7732d98092f677fedf362c7ae0d4d0386440db45
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-23 11:44:04 +0530

    configured UserNotFoundException for getter and setter methods

--- user-service/src/main/java/com/cdad/project/userservice/exceptions/UserNotFoundException.java
+++ user-service/src/main/java/com/cdad/project/userservice/exceptions/UserNotFoundException.java
@@ -1,5 +1,10 @@
 package com.cdad.project.userservice.exceptions;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserNotFoundException extends Exception {
     public UserNotFoundException(String message) {
         super(message);
