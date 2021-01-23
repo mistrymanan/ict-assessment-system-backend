74a7ec753c59bb724acde83dd8c10a361a2af298
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2021-01-23 11:56:11 +0530

    added no arg constructor.

--- user-service/src/main/java/com/cdad/project/userservice/dto/ErrorResponse.java
+++ user-service/src/main/java/com/cdad/project/userservice/dto/ErrorResponse.java
@@ -2,9 +2,11 @@ package com.cdad.project.userservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private String error;
     private String message;
