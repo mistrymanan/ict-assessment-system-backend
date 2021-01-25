2e5d35d171e634dd9703736c4a0c519c9ec6c8a0
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2021-01-25 16:58:12 +0530

    Classroom entity added

---
+++ classroom-service/src/main/java/com/cdad/project/classroomservice/classroomservice/entity/Classroom.java
@@ -0,0 +1,24 @@
+package com.cdad.project.classroomservice.classroomservice.entity;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.HashSet;
+
+@Document(collection = "classrooms")
+@Data
+public class Classroom {
+    @MongoId(FieldType.OBJECT_ID)
+    private ObjectId id;
+    private String title;
+    @Indexed(unique = true)
+    private String slug;
+    @Indexed
+    private String ownerEmail;
+    private HashSet<String> instructors;
+    private HashSet<String> enrolledUsers;
+}
