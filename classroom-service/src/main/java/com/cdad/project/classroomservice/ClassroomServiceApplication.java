9937befa535ca0e3361b3da3dfdd0c49207cad53
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-27 12:04:47 +0530

    moved ClassroomServiceApplication.java to parent package.

--- classroom-service/src/main/java/com/cdad/project/classroomservice/ClassroomServiceApplication.java
+++ classroom-service/src/main/java/com/cdad/project/classroomservice/ClassroomServiceApplication.java
@@ -1,11 +1,16 @@
-package com.cdad.project.classroomservice.classroomservice;
+package com.cdad.project.classroomservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClassroomServiceApplication {
-
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClassroomServiceApplication.class, args);
 	}
