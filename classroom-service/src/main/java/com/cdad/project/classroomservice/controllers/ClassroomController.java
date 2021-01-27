713f92c0a2bb43555ffb24cfce1ff58945257492
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2021-01-27 12:03:55 +0530

    ClassroomController added

---
+++ classroom-service/src/main/java/com/cdad/project/classroomservice/controllers/ClassroomController.java
@@ -0,0 +1,61 @@
+package com.cdad.project.classroomservice.controllers;
+
+import com.cdad.project.classroomservice.dto.ErrorResponse;
+import com.cdad.project.classroomservice.entity.Classroom;
+import com.cdad.project.classroomservice.exceptions.ClassroomAccessForbidden;
+import com.cdad.project.classroomservice.exceptions.ClassroomAlreadyExists;
+import com.cdad.project.classroomservice.exceptions.ClassroomNotFound;
+import com.cdad.project.classroomservice.exchanges.CreateClassroomRequest;
+import com.cdad.project.classroomservice.service.ClassroomService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("")
+public class ClassroomController {
+    final private ClassroomService classroomService;
+    final private ModelMapper modelMapper;
+    public ClassroomController(ClassroomService classroomService, ModelMapper modelMapper) {
+        this.classroomService = classroomService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    Classroom createClassroom(@RequestBody @Valid CreateClassroomRequest createClassroomRequest, @AuthenticationPrincipal Jwt jwt) throws ClassroomAlreadyExists {
+        return classroomService.addClassroom(createClassroomRequest,jwt);
+    }
+    @DeleteMapping("{classroomSlug}")
+    @ResponseStatus(HttpStatus.OK)
+    void deleteClassroom(@PathVariable String classroomSlug, @AuthenticationPrincipal Jwt jwt) throws ClassroomNotFound, ClassroomAccessForbidden {
+        classroomService.removeClassroom(classroomSlug, jwt);
+    }
+
+    @ExceptionHandler(ClassroomNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    ErrorResponse handleClassroomNotFound(ClassroomNotFound e){
+        ErrorResponse errorResponse=modelMapper.map(e,ErrorResponse.class);
+        errorResponse.setError("Not Found");
+        return errorResponse;
+    }
+
+    @ExceptionHandler(ClassroomAlreadyExists.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    ErrorResponse handleClassroomAlreadyExists(ClassroomAlreadyExists e){
+        ErrorResponse errorResponse=modelMapper.map(e,ErrorResponse.class);
+        errorResponse.setError("Already Exists");
+        return errorResponse;
+    }
+    @ExceptionHandler(ClassroomAccessForbidden.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    ErrorResponse handleClassroomAccessForbidden(ClassroomAccessForbidden e){
+        ErrorResponse errorResponse=modelMapper.map(e,ErrorResponse.class);
+        errorResponse.setError("Classroom Access Forbidden");
+        return errorResponse;
+    }
+}
