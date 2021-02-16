ef1b5a1f349069cbeef8700feb962a32feaf3767
Manan Mistry <mananmistry10@gmail.com>
2021-02-16 11:15:11 +0530

    exchanges added

--- user-service/src/main/java/com/cdad/project/userservice/exchanges/GetUsersDetailRequest.java
+++ user-service/src/main/java/com/cdad/project/userservice/exchanges/GetUsersDetailRequest.java
@@ -1,2 +1,14 @@
-package com.cdad.project.userservice.exchanges;public class GetUsersDetailRequest {
+package com.cdad.project.userservice.exchanges;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+
+@Data
+@NoArgsConstructor
+public class GetUsersDetailRequest {
+    @NotNull
+    private HashSet<String> usersEmail;
 }
--- user-service/src/main/java/com/cdad/project/userservice/exchanges/GetUsersDetailsResponse.java
+++ user-service/src/main/java/com/cdad/project/userservice/exchanges/GetUsersDetailsResponse.java
@@ -1,2 +1,13 @@
-package com.cdad.project.userservice.exchanges;public class GetUsersDetailsResponse {
+package com.cdad.project.userservice.exchanges;
+
+import com.cdad.project.userservice.dto.UsersDetail;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
+@Data
+@NoArgsConstructor
+public class GetUsersDetailsResponse {
+    HashMap<String, UsersDetail> usersDetail;
 }
