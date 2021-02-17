8ef0d39847da5f04fac62954699e336c91c6c0bf
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2021-02-17 13:26:41 +0530

    Added ClassroomAndUserDetailsDTO

--- classroom-service/src/main/java/com/cdad/project/classroomservice/dto/ClassroomAndUserDetailsDTO.java
+++ classroom-service/src/main/java/com/cdad/project/classroomservice/dto/ClassroomAndUserDetailsDTO.java
@@ -1,2 +1,18 @@
-package com.cdad.project.classroomservice.dto;public class ClassroomAndUserDetailsDTO {
+package com.cdad.project.classroomservice.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.cdad.project.classroomservice.serviceclient.userservice.dtos.UserDetail;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class ClassroomAndUserDetailsDTO {
+    private String title;
+    private String slug;
+    private String ownerEmail;
+    private String ownerName;
+    private List<UserDetail> instructors;
+    private List<UserDetail> enrolledUsers;
 }
