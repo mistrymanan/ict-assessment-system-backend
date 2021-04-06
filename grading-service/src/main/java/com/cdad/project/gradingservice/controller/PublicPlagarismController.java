a4966732bb2083fb39bdde456893efa45255e665
Manan Mistry <39569070+mistrymanan@users.noreply.github.com>
2021-04-06 21:14:08 +0530

    Update PublicPlagarismController.java

--- grading-service/src/main/java/com/cdad/project/gradingservice/controller/PublicPlagarismController.java
+++ grading-service/src/main/java/com/cdad/project/gradingservice/controller/PublicPlagarismController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("public")
+@RequestMapping("public-plagarism")
 public class PublicPlagarismController {
     private final SubmissionPlagarismService submissionPlagarismService;
 
