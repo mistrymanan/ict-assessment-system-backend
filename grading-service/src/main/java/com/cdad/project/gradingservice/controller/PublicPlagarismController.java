2b6507ef6ed0160d888dc5203e76eafa0a7cd607
Manan <manan.cba1718@ict.gnu.ac.in>
2021-04-06 18:33:49 +0530

    submission service ready for plagarism check

--- grading-service/src/main/java/com/cdad/project/gradingservice/controller/PublicPlagarismController.java
+++ grading-service/src/main/java/com/cdad/project/gradingservice/controller/PublicPlagarismController.java
@@ -1,10 +1,28 @@
 package com.cdad.project.gradingservice.controller;
 
+import com.cdad.project.gradingservice.dto.UserQuestionResponseDTO;
+import com.cdad.project.gradingservice.service.SubmissionPlagarismService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("public")
 public class PublicPlagarismController {
+    private final SubmissionPlagarismService submissionPlagarismService;
 
+    public PublicPlagarismController(SubmissionPlagarismService submissionPlagarismService) {
+        this.submissionPlagarismService = submissionPlagarismService;
+    }
+
+    @GetMapping("{assignmentId}/{questionId}")
+    public List<UserQuestionResponseDTO> getUserQuestionResponses(@PathVariable String assignmentId, @PathVariable String questionId,
+                                                                  @AuthenticationPrincipal Jwt jwt){
+        return this.submissionPlagarismService.getUsersQuestionResponseDTO(assignmentId,questionId,jwt);
+    }
 }
